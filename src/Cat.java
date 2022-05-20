a79447f4bd013919f42ff0012aa2a13d6d4f878b
Edgar50707 <105382844+Edgar50707@users.noreply.github.com>
2022-05-21 00:22:07 +0400

    Initial commit

---
+++ src/Cat.java
@@ -0,0 +1,76 @@
+
+public class Cat {
+    public static void main(String[] args) {
+        /*
+        boolean t = false;               //165
+        int n = 729;
+        for (int i = 1; i <= n; i *= 3) {
+            if (i == n) {
+                t = true;
+            }
+        }
+        System.out.println(t);
+
+
+
+
+        int y = 0;                     //166
+        int n1 = 256;
+        for (int i = 1; i <= n1; i *=4) {
+            if (i == n1){
+                y = 1;
+            }
+        }
+        System.out.println(y);
+
+
+        int[] array = {25, 78, 590, 365};  //215
+        int sum = 0;
+        for (int i = 0; i < array.length; i += 2) {
+            sum += array[i];
+
+        }
+        System.out.println(sum);
+
+
+
+
+        int[] array1 = {4,5,2,3,2};      //216
+        int sum1 = 1;
+        for (int i = 0; i < array1.length; i+=2) {
+            sum1 *= array1[i];
+
+        }
+        System.out.println(sum1);
+
+
+        int[] array2 = {6, 7, 4, 2, 7, 3};  //217
+        int sum2 = 1;
+        for (int i = 1; i < array2.length; i += 2) {
+            int j = array2[i] * 2;
+            sum2 *= j;
+
+        }
+        System.out.println(sum2);
+
+
+        int[] array3 = {4, -8, -9, 5, 3, 7, -7}; //220
+        int d = 0;
+        int b = 0;
+        for (int i = 0; i < array3.length; i++) {
+            if (array3[i] < 0) {
+                b++;
+            } else {
+                d++;
+            }
+        }
+        System.out.println("Drakan " + d);
+        System.out.println("Bacasakan " + b);
+
+         */
+        
+
+
+    }
+}
+
